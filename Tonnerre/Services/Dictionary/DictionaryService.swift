e2ad04cee6e2fded9bbbd3dc4bf1e85fc890e146
Yaxin Cheng <yaxin.cheng@dal.ca>
2018-06-08 16:57:52 -0300

    Add basic definition service.

---
+++ Tonnerre/Services/Dictionary/DictionaryService.swift
@@ -0,0 +1,46 @@
+//
+//  DictionaryService.swift
+//  Tonnerre
+//
+//  Created by Yaxin Cheng on 2018-06-08.
+//  Copyright © 2018 Yaxin Cheng. All rights reserved.
+//
+
+import CoreServices
+import Cocoa
+
+struct DictionarySerivce: TonnerreService {
+  let icon: NSImage = #imageLiteral(resourceName: "dictionary")
+  let content: String = "Find definition for word in dictionary"
+  let name: String = "Define words..."
+  let keyword: String = "define"
+  let arguments: [String] = ["word"]
+  let acceptsInfiniteArguments: Bool = true
+  let hasPreview: Bool = false
+  
+  func prepare(input: [String]) -> [Displayable] {
+    let query = input.joined(separator: " ")
+    var queriedResult: BaseDisplayItem<URL> = BaseDisplayItem<URL>(name: query, content: "\"\(query)\" is not found", icon: icon)
+    autoreleasepool {
+      let termRange = DCSGetTermRangeInString(nil, query as CFString, 0)
+      if termRange.location != kCFNotFound {
+        if let definition = DCSCopyTextDefinition(nil, query as CFString, termRange)?.takeRetainedValue() {
+          let (startIndex, endIndex) = (query.index(query.startIndex, offsetBy: termRange.location),
+                                        query.index(query.startIndex, offsetBy: termRange.length))
+          let foundTerm = String(query[startIndex..<endIndex])
+          let urlEncoded = foundTerm.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? foundTerm
+          let dictURL = URL(string: String(format: "dict://%@", urlEncoded))!
+          queriedResult = BaseDisplayItem(name: foundTerm, content: definition as String, icon: icon, innerItem: dictURL)
+        }
+      } else {
+        queriedResult = BaseDisplayItem<URL>(name: query, content: "\"\(query)\" is not found", icon: icon)
+      }
+    }
+    return [queriedResult]
+  }
+  
+  func serve(source: Displayable, withCmd: Bool) {
+    guard let request = (source as? BaseDisplayItem<URL>)?.innerItem else { return }
+    NSWorkspace.shared.open(request)
+  }
+}
