7aa450174936a4aa1644f25fe26ea59c2f2b0225
Yaxin Cheng <yaxin.cheng@dal.ca>
2018-08-05 22:44:11 -0400

    Ready to build the setting panel

---
+++ SettingPanel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SettingPanel
+//
+//  Created by Yaxin Cheng on 2018-08-01.
+//  Copyright © 2018 Yaxin Cheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ SettingPanel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SettingPanel
+//
+//  Created by Yaxin Cheng on 2018-08-01.
+//  Copyright © 2018 Yaxin Cheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
