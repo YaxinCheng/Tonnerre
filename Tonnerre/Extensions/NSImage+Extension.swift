6237a09ee07189815b7f31556cbfae859c03db40
Yaxin Cheng <yaxin.cheng@dal.ca>
2018-09-27 18:01:38 -0400

    Cleaned up code and unused images

--- Tonnerre/Extensions/NSImage+Extension.swift
+++ Tonnerre/Extensions/NSImage+Extension.swift
@@ -13,10 +13,6 @@ extension NSImage {
     return NSImage(contentsOfFile: "/Applications/Safari.app/Contents/Resources/compass.icns") ?? #imageLiteral(resourceName: "notFound").tintedImage(with: TonnerreTheme.current.imgColour)
   }
   
-  static var screenLock: NSImage {
-    return NSImage(contentsOfFile: "/System/Library/PreferencePanes/DesktopScreenEffectsPref.prefPane/Contents/Resources/DesktopScreenEffectsPref.icns") ?? #imageLiteral(resourceName: "notFound").tintedImage(with: TonnerreTheme.current.imgColour)
-  }
-  
   static var terminal: NSImage {
     return NSImage(contentsOfFile: "/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns") ?? #imageLiteral(resourceName: "notFound").tintedImage(with: TonnerreTheme.current.imgColour)
   }
@@ -31,17 +27,5 @@ extension NSImage {
   
   static var calculator: NSImage {
     return NSImage(contentsOfFile: "/Applications/Calculator.app/Contents/Resources/AppIcon.icns") ?? #imageLiteral(resourceName: "notFound").tintedImage(with: TonnerreTheme.current.imgColour)
-  }
-  
-  static var trash: NSImage {
-    let isDark = TonnerreTheme.current == .dark
-    let baseURL = URL(fileURLWithPath: "/System/Library/CoreServices/Dock.app/Contents/Resources/")
-    var fileName = "trash%@@2x.png"
-    switch isDark {
-    case true:  fileName = String(format: fileName, "full2")
-    case false: fileName = String(format: fileName, "full")
-    }
-    let imageURL = baseURL.appendingPathComponent(fileName)
-    return NSImage(contentsOf: imageURL) ?? #imageLiteral(resourceName: "notFound@2x.png").tintedImage(with: TonnerreTheme.current.imgColour)
   }
 }
